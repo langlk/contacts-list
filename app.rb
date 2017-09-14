7b839a80424e40241661e91e64ac6ad6ca973419
Kelsey Langlois and Snow Vilay <snow@>
2017-09-14 16:01:07 -0700

    add user interface to add additional phone numbers, emails and addresses.

--- app.rb
+++ app.rb
@@ -43,34 +43,46 @@ post('/contact/:name_route') do
   @contact.company = params["company"]
   @contact.contact_type = params["contact-type"]
 
-  phone_number = params["phone-number"]
-  if phone_number.length != 0
-    phone_type = params["phone-type"]
-    if phone_type.length == 0
-      @contact.add_phone(phone_number)
-    else
-      @contact.add_phone(phone_number, phone_type)
+  count = 0
+  while params.has_key?("phone-number-" + count.to_s)
+    phone_number = params["phone-number-" + count.to_s]
+    if phone_number.length != 0
+      phone_type = params["phone-type-" + count.to_s]
+      if phone_type.length == 0
+        @contact.add_phone(phone_number)
+      else
+        @contact.add_phone(phone_number, phone_type)
+      end
     end
+    count += 1
   end
 
-  email = params["email"]
-  if email.length != 0
-    email_type = params["email-type"]
-    if email_type.length == 0
-      @contact.add_email(email)
-    else
-      @contact.add_email(email, email_type)
+  count = 0
+  while params.has_key?("email-" + count.to_s)
+    email = params["email-" + count.to_s]
+    if email.length != 0
+      email_type = params["email-type-" + count.to_s]
+      if email_type.length == 0
+        @contact.add_email(email)
+      else
+        @contact.add_email(email, email_type)
+      end
     end
+    count += 1
   end
 
-  address = params["address"]
-  if address.length != 0
-    address_type = params["address-type"]
-    if address_type.length == 0
-      @contact.add_address(address)
-    else
-      @contact.add_address(address, address_type)
+  count = 0
+  while params.has_key?("address-" + count.to_s)
+    address = params["address-" + count.to_s]
+    if address.length != 0
+      address_type = params["address-type-" + count.to_s]
+      if address_type.length == 0
+        @contact.add_address(address)
+      else
+        @contact.add_address(address, address_type)
+      end
     end
+    count += 1
   end
   erb(:contact)
 end
