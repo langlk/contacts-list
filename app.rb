f99e83831d87b6f7d3c032eddb092b077dad8f5c
Kelsey Langlois and Snow Vilay <kelsey@>
2017-09-14 13:40:29 -0700

    Update list page to add new contacts on form post.

--- app.rb
+++ app.rb
@@ -4,7 +4,21 @@ also_reload 'lib/**/*.rb'
 require './lib/contact'
 require 'pry'
 
-get("/")do
+get("/") do
+  @contacts = ContactList::Contact.all
+  erb(:contact_list)
+end
+
+post('/') do
+  attributes = {
+    :first_name => params["first-name"],
+    :last_name => params["last-name"],
+    :job_title => params["job-title"],
+    :company => params["company"],
+    :contact_type => params["contact-type"]
+  }
+  contact = ContactList::Contact.new(attributes)
+  contact.save
   @contacts = ContactList::Contact.all
   erb(:contact_list)
 end
