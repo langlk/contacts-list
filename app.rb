f0ed8ad7b3f086f054d1076784275607fd0132b7
Kelsey Langlois and Snow Vilay <snow@>
2017-09-14 14:22:26 -0700

    Add individual contact page and contact edit page.

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ also_reload 'lib/**/*.rb'
 require './lib/contact'
 require 'pry'
 
+bob = ContactList::Contact.new({:first_name => "Bob", :last_name => "Bobbington", :job_title => "Developer", :company => "Microsoft", :contact_type => "Professional"})
+bob.add_phone("555-555-5555", "work")
+bob.add_phone("111-111-1111", "cell")
+bob.add_address("1234 Redmond Way, Redmond, WA 99999", "home")
+bob.add_address("466 Microsoft Way, Redmond, WA 99999", "work")
+bob.add_email("bob@email.com", "personal")
+bob.save
+
 get("/") do
   @contacts = ContactList::Contact.all
   erb(:contact_list)
@@ -21,4 +29,22 @@ post('/') do
   contact.save
   @contacts = ContactList::Contact.all
   erb(:contact_list)
+end
+
+get('/contact/:name_route') do
+  name = params[:name_route].split('_').join(' ')
+  @contact = ContactList::Contact.find(name)
+  erb(:contact)
+end
+
+post('/edit/:name_route') do
+  name = params[:name_route].split('_').join(' ')
+  @contact = ContactList::Contact.find(name)
+  erb(:edit)
+end
+
+post('/contact/:name_route') do
+  name = params[:name_route].split('_').join(' ')
+  @contact = ContactList::Contact.find(name)
+  erb(:contact)
 end
